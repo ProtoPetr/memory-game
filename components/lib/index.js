808f30f60466e3484a1fde119f110754769a18b2
ProtoPetr <protopopovpetr@gmail.com>
2021-02-04 03:11:01 +0300

    wrote a main logic

---
+++ components/lib/index.js
@@ -0,0 +1,10 @@
+import * as R from 'rambda'
+
+export function allEquals(xs) {
+    // Vacuous truth
+    if (xs.length < 2) {
+        return true
+    }
+    let [head, ...tail] = xs
+    return R.all(R.equals(head), tail)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,119 @@
-import React from 'react'
+import * as R from 'rambda'
+import React, { useEffect, useState } from 'react'
 import * as Cell from '../components/Cell'
+import * as Board from '../components/Board'
 
 export default function Layout() {
+  return <>
+  <GameView/>
+  </>
+}
 
-let cellOpen = {symbol: 'A', status: Cell.Status.Open}
-let cellClosed = {symbol: 'B', status: Cell.Status.Closed}
-let cellFailed = {symbol: 'C', status: Cell.Status.Failed}
-let cellDone = {symbol: 'D', status: Cell.Status.Done}
+// LOGIC ===========================================================================================
 
-  return <div>
-    <Cell.View cell={cellOpen}/>
-    <Cell.View cell={cellClosed}/>
-    <Cell.View cell={cellFailed}/>
-    <Cell.View cell={cellDone}/>
+let Status = {
+  Stopped: 'Stopped',
+  Running: 'Running',
+  Won: 'Won',
+  Lost: 'Lost',
+}
+
+let initialBoard = [
+  {symbol: 'A', status: Cell.Status.Closed},
+  {symbol: 'A', status: Cell.Status.Closed},
+  {symbol: 'B', status: Cell.Status.Closed},
+  {symbol: 'B', status: Cell.Status.Closed},
+  {symbol: 'C', status: Cell.Status.Closed},
+  {symbol: 'C', status: Cell.Status.Closed},
+    ]
+
+let startGame = (state) => ({
+  board: initialBoard,
+  status: Status.Running,
+})
+
+let openCell = R.curry((i, state) => ({
+  ...state, 
+  board: Board.setStatusAt(i, Cell.Status.Open, state.board),
+}))
+
+let succeedStep = (state) => ({
+  ...state,
+  board: Board.setStatusesBy(Cell.isOpen, Cell.Status.Done, state.board)
+})
+
+let failStep1 = (state) => ({
+  ...state,
+  board: Board.setStatusesBy(Cell.isOpen, Cell.Status.Failed, state.board)
+})
+
+let failStep2 = (state) => ({
+  ...state,
+  board: Board.setStatusesBy(Cell.isFailed, Cell.Status.Closed, state.board)
+})
+
+function GameView() {
+  let [state, setState] = useState({
+    board: initialBoard,
+    status: Status.Running,
+  })
+
+let {board, status} = state
+
+function handleStartingClick(i) {
+  if (status != Status.Running) {
+    setState(startGame)
+  }
+}
+
+// Board handling
+useEffect(_ =>{
+  if (Board.areOpensEqual(board)) {
+    setState(succeedStep)
+  } else if (Board.areOpensDifferent(board)) {
+    setState(failStep1)
+    setTimeout(_ => {
+      setState(failStep2)
+    }, 500)
+  }
+}, [board])
+
+  return <div onClick={handleStartingClick}>
+    <ScreenBoxView status={status} board={board}/>
   </div>
 }
+
+function ScreenBoxView({status, board}) {
+  switch (status) {
+    case Status.Running:
+      return <Board.BoardView board = {board} onClickAt={() => null}/>
+
+    case Status.Stopped:
+      return <Board.ScreenView className='gray'>
+        <div>
+          <h1>Memory game</h1>
+          <p className='small' style={{textAlign: 'center'}}>Click anywhere to start!</p>
+        </div>
+      </Board.ScreenView>
+
+    case Status.Won:
+      return <Board.ScreenView className='green'>
+      <div>
+        <h1>Victory!</h1>
+        <p className='small' style={{textAlign: 'center'}}>Click anywhere to try again!</p>
+      </div>
+    </Board.ScreenView>
+
+    case Status.Lost:
+      return <Board.ScreenView className='red'>
+      <div>
+        <h1>Defeat!</h1>
+        <p className='small' style={{textAlign: 'center'}}>Click anywhere to try again!</p>
+      </div>
+    </Board.ScreenView>
+  }
+}
+
+
+
+
